5ef506378376553685c0834db93b4771c8fdf0f5
robbcorrea <51095085+RobbCorrea@users.noreply.github.com>
2020-03-31 16:43:52 -0600

    Add user.js to controllers and user.js to routers

---
+++ controllers/user.js
@@ -0,0 +1,11 @@
+const bcrypt = require("bcrypt-nodejs");
+const User = require("../models/user");
+//const jwt = require("")
+
+function signUp(req, res) {
+  console.log("An Endpoint of signUp");
+}
+
+module.exports = {
+  signUp
+};
---
+++ routers/user.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const UserController = require("../controllers/user");
+
+const api = express.Router();
+
+//Whenever a post is made it will execute the function signUp by means of UserController
+api.post("/sign-up", UserController.signUp);
+
+module.exports = api;
